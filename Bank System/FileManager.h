c7c7ba4786d7031073a71689668c993541282e2b
mariam <mariamahmedmostafa18@gmail.com>
2025-11-01 23:55:49 +0200

    class file manger

--- Bank System/FileManager.h
+++ Bank System/FileManager.h
@@ -1,16 +1,60 @@
 #pragma once
 #include "DataSourceInterface.h"
-class FileManager :
-    public DataSourceInterface
-{
+#include "Client.h"
+#include "Employee.h"
+#include "Admin.h"
+#include "FilesHelper.h"
+#include <iostream>
+#include <vector>
+using namespace std;
 
-    //implementation of every method in DataSourceInterface
+#define CLIENTS_FILE "Clients.txt"
+#define EMPLOYEES_FILE "Employees.txt"
+#define ADMINS_FILE "Admins.txt"
+#define CLIENT_LAST_ID_FILE "Client_Last_ID.txt"
+#define EMPLOYEE_LAST_ID_FILE "Employee_Last_ID.txt"
+#define ADMIN_LAST_ID_FILE "Admin_Last_ID.txt"
 
-   /* addClient should save client info in clients.txt.
-        addEmployee should save employee info in employees.txt.
-        addAdmin should save employee info in admins.txt.
-        getAllClients(), getAllEmployees(), and getAllAdmins() should retrieve data from files.
-        removeAllClients(), removeAllEmployees(), and removeAllAdmins() should remove all data from the files.*/
+class FileManager :public DataSourceInterface {
+public:
+    void addClient(Client &obj) override {
+        FilesHelper::saveClient(obj);
+    }
 
+    void addEmployee(Employee &obj) override {
+        FilesHelper::saveEmployee(obj);
+    }
+
+    void addAdmin(Admin &obj) override {
+        FilesHelper::saveEmployee(obj);
+    }
+
+     vector<Client> getClients() override {
+        return FilesHelper::getClients();
+    }
+
+
+    
+    vector<Employee> getEmployees() {
+        vector<Employee> employees = FilesHelper::getEmployees();
+        return employees;
+    }
+  
+
+   vector<Admin> getAdmins()override {
+    
+        return admins;
+    }
+
+    void removeAllClients() {
+   /*     FilesHelper::clearFile(CLIENTS_FILE, CLIENT_LAST_ID_FILE);*/
+    }
+
+    void removeAllEmployees() {
+        //F/*ilesHelper::clearFile(EMPLOYEES_FILE, EMPLOYEE_LAST_ID_FILE);*/
+    }
+
+    void removeAllAdmins()  {
+      /*  FilesHelper::clearFile(ADMINS_FILE, ADMIN_LAST_ID_FILE);*/
+    }
 };
-
