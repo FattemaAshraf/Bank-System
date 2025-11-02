63ca45503ed77e5f2beee3fc9bd3299f93f89135
Salma Anany <salmayousry5@gmail.com>
2025-11-02 05:36:43 -0800

    - Implemented addClient() to add clients to static collection    - Implemented getClientById() to search clients by ID    - Implemented getAllClients() to retrieve all clients    - Added static vector to store all Client instances

--- Bank System/Client.cpp
+++ Bank System/Client.cpp
@@ -1,1 +1,20 @@
 #include "Client.h"
+
+vector<Client> Client::allClients;
+
+void Client::addClient(const Client& client) {
+    allClients.push_back(client);
+}
+
+Client* Client::getClientById(int id) {
+    for (int i = 0; i < allClients.size(); i++) {
+        if (allClients[i].getId() == id) {
+            return &allClients[i];
+        }
+    }
+    return nullptr;
+}
+
+vector<Client>& Client::getAllClients() {
+    return allClients;
+}
