737cba56104f3813001a738db78876d8f65538a5
Helana Maged <helanamaged21@gmail.com>
2025-11-20 20:02:05 +0200

    screen

---
+++ Bank System/Screen.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Screen {
+public:
+    //d. e. f. g methods >>        
+    static int loginAs();                  
+    static void invalid(int c);            
+    static void logout();                  
+    static void loginScreen(int c);        
+                    
+};
